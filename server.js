6bf4336d4526b758a06d16b9dbeeb138caff4530
HZreal <huangzhen_happy@163.com>
2022-10-11 21:51:47 +0800

    'first commit'

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express()
+
+
+app.get('/get', (req, res) => {
+    console.log(req)
+    res.send({code: 0, msg: 'success', data: 'data'})
+})
+
+app.post('/post', (req, res) => {
+    console.log(req)
+    res.send({code: 0, msg: 'success', data: 'data'})
+})
+
+app.listen(80, () => {
+
+})
+
+
